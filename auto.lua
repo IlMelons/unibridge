5b5337e6ebb0ea27a370812c064d8a4b4620c77a
IlMelons <melonss2002@gmail.com>
2024-11-20 20:53:10 +0100

    New Setup Test

--- auto.lua
+++ auto.lua
@@ -1,39 +1,59 @@
 if GetResourceState("ox_lib") ~= "started" then return print("[ERROR]: ox_lib not found, unable to continue") end
 
 Config = lib.load("config")
+
 ResourceFinder = {}
 
+---@description Supported Framework List
+Frameworks = {
+    {name = "esx", resource = "es_extended"},
+    {name = "qb", resource = "qb-core"},
+    {name = "nd", resource = "ND_Core"},
+    {name = "qbx", resource = "qbx_core"},
+    {name = "ox", resource = "ox_core"},
+}
+
+---@description Supported Target List
+Targets = {
+    {name = "qb", resource = "qb-target"},
+    {name = "ox", resource = "ox_target"},
+}
+
+---@description Supported Inventory List
+Inventories = {
+    {name = "qb", resource = "qb-inventory"},
+    {name = "ox", resource = "ox_inventory"},
+}
+
 ---@description Auto-Detect Framework Function
-if (Config.Framework == "auto" and GetResourceState("es_extended") == "started") or Config.Framework == "esx" then
-    ResourceFinder.Framework = "esx"
-elseif (Config.Framework == "auto" and GetResourceState("qb-core") == "started") or Config.Framework == "qb" then
-    ResourceFinder.Framework = "qb"
-elseif (Config.Framework == "auto" and GetResourceState("qbx_core") == "started") or Config.Framework == "qbx" then
-    ResourceFinder.Framework = "qbx"
-elseif (Config.Framework == "auto" and GetResourceState("ox_core") == "started") or Config.Framework == "ox" then
-    ResourceFinder.Framework = "ox"
-else
-    ResourceFinder.Framework = "notfound"
+for i = 1, #Frameworks do
+    local frameworkName, frameworkFolder = Frameworks[i].name, Frameworks[i].resource
+    
+    if (Config.Framework == "auto" and GetResourceState(frameworkFolder) == "started") or Config.Framework == frameworkName then
+        ResourceFinder.Framework = frameworkName
+        break
+    end
 end
 
 ---@description Auto-Detect Target Function
-if (Config.Target == "auto" and GetResourceState("ox_target") == "started") or Config.Target == "ox" then
-    ResourceFinder.Target = "ox"
-elseif (Config.Target == "auto" and GetResourceState("qb-target") == "started") or Config.Target == "qb" then
-    ResourceFinder.Target = "qb"
-else
-    ResourceFinder.Target = "notfound"
+for i = 1, #Targets do
+    local targetName, targetFolder = Targets[i].name, Targets[i].resource
+    
+    if (Config.Target == "auto" and GetResourceState(targetFolder) == "started") or Config.Target == (target or targetLabel) then
+        ResourceFinder.Target = targetName
+        break
+    end
 end
 
 ---@description Auto-Detect Inventory Function
-if (Config.Inventory == "auto" and GetResourceState("ox_inventory") == "started") or Config.Inventory == "ox" then
-    ResourceFinder.Inventory = "ox"
-elseif (Config.Inventory == "auto" and GetResourceState("qb-inventory") == "started") or Config.Inventory == "qb" then
-    ResourceFinder.Inventory = "qb"
-else
-    ResourceFinder.Inventory = "notfound"
+for i = 1, #Inventories do
+    local inventoryName, inventoryFolder = Inventories[i].name, Inventories[i].resource
+    
+    if (Config.Inventory == "auto" and GetResourceState(inventoryFolder) == "started") or Config.Inventory == inventoryName then
+        ResourceFinder.Inventory = inventoryName
+        break
+    end
 end
-
 
 ---@description Debug Functions - Do Not Remove
 lib.print.debug("Framework Set/Found: "..ResourceFinder.Framework)
