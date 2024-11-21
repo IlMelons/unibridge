b891b335100b9ade5c629081cbe4b8f1b2ee903a
IlMelons <melonss2002@gmail.com>
2024-11-21 13:53:54 +0100

    Optimization caching

--- autodetect.lua
+++ autodetect.lua
@@ -1,27 +1,12 @@
 if GetResourceState("ox_lib") ~= "started" then return print("[ERROR]: ox_lib not found, unable to continue") end
 
 Config = lib.load("config")
-
----@description Supported Resources List
-Frameworks = {
-    {name = "esx", resource = "es_extended"},
-    {name = "qbx", resource = "qbx_core"},
-    {name = "qb", resource = "qb-core"},
-    {name = "nd", resource = "ND_Core"},
-    {name = "ox", resource = "ox_core"},
-}
-Targets = {
-    {name = "ox", resource = "ox_target"},
-    {name = "qb", resource = "qb-target"},
-}
-Inventories = {
-    {name = "ox", resource = "ox_inventory"},
-    {name = "qb", resource = "qb-inventory"},
-}
+Supported = lib.load("supported")
 
 function DetectResource(configKey, resourceList)
-    for i = 1, #resourceList do
-        local resourceName, resourceFolder = resourceList[i].name, resourceList[i].resource
+    local resources = Supported[resourceList]
+    for i = 1, #resources do
+        local resourceName, resourceFolder = resources[i].name, resources[i].resource
         if (Config[configKey] == "auto" and GetResourceState(resourceFolder) == "started") or Config[configKey] == resourceName then
             return resourceName
         end
