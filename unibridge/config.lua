53389ccb2cf1101dca53188ee6b79de37ed77aec
IlMelons <melonss2002@gmail.com>
2024-11-21 15:37:51 +0100

    Delete unibridge directory

--- unibridge/config.lua
+++
@@ -1,8 +0,0 @@
-return {
-    Framework = "auto", -- "auto", "esx", "qb", "qbx", "ox"
-    Target = "auto", -- "auto", "ox", "qb"
-    Inventory = "auto", -- "auto", "ox", "qb"
-}
-
--- FOR DEBUG FEATURES USE THE FOLLOWING IN server.cfg
--- set ox:printlevel:unibridge "debug"
